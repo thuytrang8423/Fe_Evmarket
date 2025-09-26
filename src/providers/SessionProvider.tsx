2459e058c0a9732edc450982f800da1ebdb293d5
Miru UwU <miruhinana@gmail.com>
2025-09-26 04:09:00 +0000

    fix

--- src/providers/SessionProvider.tsx
+++ src/providers/SessionProvider.tsx
@@ -1,14 +1,16 @@
-"use client"
-import { SessionProvider } from 'next-auth/react'
-import React from 'react'
+"use client";
+import { SessionProvider } from "next-auth/react";
+import React from "react";
+import { I18nProvider } from "./I18nProvider";
 
-export default function AppSessionProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <SessionProvider>
-      {children}
-    </SessionProvider>
-  )
+export default function AppSessionProvider({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <SessionProvider>
+            <I18nProvider>{children}</I18nProvider>
+        </SessionProvider>
+    );
 }
-
-
-
